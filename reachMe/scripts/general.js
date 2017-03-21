72e37abbc3d6af0611e7e12e6b9217d5bc29e176
Aakruti Nirmal <aakrutinirmal@Aakrutis-MacBook-Pro.local>
2017-03-20 22:20:59 -0400

    web design

---
+++ reachMe/scripts/general.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	 $("a.mobile").click(function(){
+	 	$(".sidebar").slideToggle('fast');
+	 });
+
+
+window.onresize=function(event){
+if($(window).width() > 320){
+	$(".sidebar").show();
+}
+
+};
+
+
+
+
+});
